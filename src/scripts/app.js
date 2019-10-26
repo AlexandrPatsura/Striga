4d51e78577a64983c2e3f600840e5b6c1ffcfeb7
Alexandr Patsura <alexandrpatsura@gmail.com>
2019-10-27 00:30:39 +0300

    update section reviews

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,5 +7,9 @@ preloader();
 $('.slider__video').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 1,
+  variableWidth: true,
+  adaptiveHeight: true,
+  prevArrow: '<button class="slider__btn-prev"><img src="./assets/images/arrow-prev.png" alt=""></button>',
+  nextArrow: '<button class="slider__btn-next"><img src="./assets/images/arrow-next.png" alt=""></button>',
 });
